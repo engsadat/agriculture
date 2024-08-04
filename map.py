b0d3aa3c9d8f18a296f438ba57f99d93c9df936e
Elsadat Ahmed <168622396+engsadat@users.noreply.github.com>
2024-08-04 21:28:12 +0300

    Update map.py

--- map.py
+++ map.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-df = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
+df = pd.read_excel('2024-08-03_xy_wadiAbha_correct.xlsx')
 
-st.map(df)
+st.map(data=df, *, latitude=lat, longitude=lon, color=None, size=None, zoom=None, use_container_width=True)
