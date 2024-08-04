d07606c219826ca1c4cd15eff7d59c3d16a7e52c
Elsadat Ahmed <168622396+engsadat@users.noreply.github.com>
2024-08-04 21:59:05 +0300

    Update map.py

--- map.py
+++ map.py
@@ -5,4 +5,4 @@ import numpy as np
 
 df = pd.read_excel('2024-08-03_xy_wadiAbha_correct.xlsx')
 
-st.map(data=df, latitude='latitude', longitude='longitude',use_container_width=True)
+st.map(data=df, latitude=latitude, longitude=longitude,use_container_width=True)
